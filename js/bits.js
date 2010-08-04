3654078d53d3395e51b06dc8459d3b930177aaa8
Matthew Taylor <rhyolight@gmail.com>
2010-08-03 18:37:37 -0700

    First commit of stuff that sortof works.

---
+++ js/bits.js
@@ -0,0 +1,131 @@
+$(function () {
+    
+    window.BITS = {
+        cfg: {},
+        init: function(cfg) {
+            this.cfg = cfg;
+            init(this.cfg.bg);
+        },
+        isPaused: function() {
+            return this.cfg.pause;
+        },
+        pause: function() {
+            this.cfg.pause = true;
+        },
+        resume: function() {
+            this.cfg.pause = false;
+        },
+        set: function(key, val) {
+            this.cfg[key] = val;
+        }
+    };
+    
+	var c = document.getElementById('bits'),
+		ctx = c.getContext('2d'),
+		h, w, limit,
+		shapes = {
+			circle: drawLittleCircle, 
+			square: drawLittleSquare
+		},
+		i, j, clr;
+		
+	function init(cfg) {
+		w = c.width = window.innerWidth;
+		h = c.height = window.innerHeight;
+		limit = h > w ? h : w;
+		draw(cfg);
+		setInterval(draw, BITS.cfg.every);
+	}
+		
+	function draw(cfg) {
+
+		cfg = cfg || BITS.cfg;
+		
+		if (BITS.isPaused()) {
+			return;
+		}
+		w = c.width;
+		h = c.height;
+		limit = h > w ? h : w;
+		
+		if (cfg.gridlock) {
+    		drawGridlock(cfg);
+		} else {
+		    drawRandom(cfg);
+		}
+		
+	}
+
+    function drawGridlock(cfg) {
+        var clr = cfg.clr1, i, j, x, y, stroke;
+        for (i=0; i<limit; i=i+cfg.width) {
+            for (j=0; j<limit; j=j+cfg.width) {
+                stroke = cfg.stroke;
+                clr = clr === cfg.clr1 ? cfg.clr2 : cfg.clr1;
+                drawLittleSquare(clr, j, i, cfg.width, stroke);
+            }
+            if (!cfg.striped) {
+                clr = clr === cfg.clr1 ? cfg.clr2 : cfg.clr1;
+            }
+        }
+    }
+    
+    function drawRandom(cfg) {
+        var i, x, y, stroke;
+        for (i=0; i<cfg.times; i++) {
+			x = Math.round(Math.random()*limit);
+			y = Math.round(Math.random()*limit);
+			clr = Math.random() < 0.5 ? cfg.clr1 : cfg.clr2;
+			stroke = clr === cfg.clr1 ? cfg.clr2 : cfg.clr1
+			if (shapes[cfg.shape] instanceof Function) {
+			    shapes[cfg.shape](clr, x, y, cfg.width, cfg.stroke);
+		    }
+		}
+    }
+	
+	function drawLittleCircle(clr, x, y, w, stroke) {
+		ctx.beginPath();
+		ctx.arc(x,y, w/2, 0, Math.PI*2, true);
+		ctx.closePath();
+		if (stroke) {
+			ctx.strokeStyle = stroke;
+			ctx.stroke();
+		}
+		ctx.fillStyle = clr;
+		ctx.fill();
+	}
+	
+	function drawLittleSquare(clr, x, y, w, stroke) {
+		if (stroke) {
+			ctx.strokeStyle = stroke;
+			ctx.strokeRect(x, y, w, w);
+		}
+		ctx.fillStyle = clr;
+		ctx.fillRect(x, y, w, w);
+	}
+	
+	c.addEventListener('mousemove', mouseMove, false);
+	
+	function mouseMove(e) {
+	    var cell = getCursorPosition(e);
+	    drawLittleSquare('red', cell[0], cell[1], 10, 'black');
+	}
+	
+    function getCursorPosition(e) {
+        var x;
+        var y;
+        if (e.pageX || e.pageY) {
+            x = e.pageX;
+            y = e.pageY;
+        }
+        else {
+            x = e.clientX + document.body.scrollLeft +
+            document.documentElement.scrollLeft;
+            y = e.clientY + document.body.scrollTop +
+            document.documentElement.scrollTop;
+        }
+        x -= c.offsetLeft;
+        y -= c.offsetTop;
+        return [x,y];
+    }
+});
---
+++ js/jquery.swingers.js
@@ -0,0 +1,100 @@
+jQuery.fn.swing = function(cfg) {
+    
+    var $triggers = jQuery(cfg.trigger),
+        $swingers = this,
+        selectedIndex = null,
+        animQueue = [],
+        firstSwing = true,
+        leftMod = cfg.left ? leftMod(cfg.left) : 0;
+        easing = cfg.easing || 'easeOutBack';
+    
+    function leftMod(val) {
+        if (val.substr(0,1) === '+') {
+            if (val.substr(val.length-2,2) === 'px') {
+                val = val.substr(1,val.length-3);
+            }
+            return new Number(val);
+        }
+        return 0;
+    }
+    
+    function right(after, speed) {
+        var $it = jQuery(this),
+            left = window.outerWidth + 'px',
+            top = cfg.top || (window.outerHeight / 2 - $it .height() / 2) + 'px';
+        speed = speed || 'slow';
+        $it.animate({left: left, top: top}, speed, easing, after);
+    }
+    
+    function left(after, speed) {
+        var $it = jQuery(this),
+            left = '-' + ($it.width()+10) + 'px',
+            top = cfg.top || (window.outerHeight / 2 - $it .height() / 2) + 'px';
+        speed = speed || 'slow';
+        $it.animate({left: left, top: top}, speed, easing, after);
+    }
+    
+    function center(after, speed) {
+        var $el = jQuery(this),
+            top = cfg.top || (window.outerHeight / 2 - $el.height() / 2) + 'px',
+            left = ((window.outerWidth / 2 - $el.width() / 2) + leftMod) + 'px';
+        speed = speed || 'slow';
+        $el.show();
+        $el.animate({
+            top: top,
+            left: left
+        }, speed, easing, after);
+    }
+    
+    function processQueue() {
+        if (animQueue.length) {
+            var cmd = animQueue.shift();
+            console.log('moving ' + $(cmd.scope).attr('class') + ' to ' + cmd.fn.name);
+            cmd.fn.call(cmd.scope, processQueue, cmd.speed);
+        }
+    }
+    
+    function select() {
+        var $s, i, inc, bumper, anim, oldIndex = selectedIndex || 0;
+        
+        selectedIndex = $swingers.index(this);
+        
+        if (oldIndex !== selectedIndex) {
+            if (oldIndex < selectedIndex) {
+                inc = 1;
+                anim = left;
+            } else if (oldIndex > selectedIndex){
+                inc = -1
+                anim = right;
+            }
+            animQueue.push({fn: anim, scope: $swingers.get(oldIndex), speed: 'fast'});
+
+            for (i = (oldIndex+inc); i !== selectedIndex; i=i+inc) {
+                $s = $swingers.get(i);
+                animQueue.push({fn: anim, scope: $s, speed: 'fast'});
+            }
+        }
+        
+        if (firstSwing) {
+            for (i = selectedIndex+1; i < $swingers.length; i++) {
+                $s = $swingers.get(i);
+                animQueue.push({fn: right, scope: $s, speed: 'fast'});
+            }
+            firstSwing = false;
+        }
+        
+        animQueue.push({fn: center, scope: this});
+        
+        processQueue();
+
+    }
+    
+    $triggers.click(function(e) {
+        var id = jQuery(e.target).attr('href').substr(1);
+        $swingers.filter('.' + id + '.swinger').each(select);
+    });
+    
+    $swingers.css('left', window.outerWidth + 'px');
+    select.call($swingers.filter(cfg.selected));
+
+};
